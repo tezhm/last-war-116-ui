b701de3d3a37433bf70c29fcbb809d75dbcf88f4
Terence Marks <tezmarks@gmail.com>
2024-04-21 14:34:19 +1000

    Adding loading overlay

--- src/pages/Schedule.tsx
+++ src/pages/Schedule.tsx
@@ -1,3 +1,7 @@
+import Backdrop from "@mui/material/Backdrop";
+import CircularProgress from "@mui/material/CircularProgress";
+import Fade from "@mui/material/Fade";
+import Modal from "@mui/material/Modal";
 import Paper from "@mui/material/Paper";
 import { styled } from "@mui/material/styles";
 import Table from "@mui/material/Table";
@@ -7,9 +11,9 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import { JSX, useEffect, useState } from "react";
+import { ApiClient } from "../components/clients/ApiClient";
 import { Dashboard } from "../components/layout/Dashboard";
 import { Title } from "../components/titles/Titles";
-import { ApiClient } from "../components/clients/ApiClient";
 
 const timezone = (new Date()).toTimeString().substring(9);
 
@@ -77,6 +81,8 @@ export function Schedule(props: ScheduleProps): JSX.Element {
     });
 
     useEffect(() => {
+        setState({ rows: [], loading: true });
+
         const currentTime = new Date();
         const start = currentTime.getTime();
         const end = start + (24 * 60 * 60 * 1000);
@@ -87,30 +93,63 @@ export function Schedule(props: ScheduleProps): JSX.Element {
         });
     }, [props]);
 
+    const indicatorSize = 80;
+
     return (
         <Dashboard pageTitle={props.title.name} currentUrl={props.title.url}>
-            <Paper sx={{ width: "100%", mb: 2, overflow: "hidden", height: "calc(100vh - 200px)" }}>
+            <Paper sx={{
+                width: "100%",
+                mb: 2,
+                overflow: "hidden",
+                height: "calc(100vh - 200px)"
+            }}>
+                <Modal
+                    aria-labelledby="transition-modal-title"
+                    aria-describedby="transition-modal-description"
+                    open={state.loading}
+                    closeAfterTransition
+                    slots={{ backdrop: Backdrop }}
+                    slotProps={{
+                      backdrop: {
+                        timeout: 500,
+                      },
+                    }}
+                    disableAutoFocus={true}
+                >
+                    <Fade in={state.loading}>
+                        <CircularProgress
+                            color="inherit"
+                            sx={{
+                                position: "absolute",
+                                top: "50%",
+                                left: "50%",
+                                marginTop: `${-indicatorSize / 2}px`,
+                                marginLeft: `${-indicatorSize / 2}px`
+                            }}
+                        />
+                    </Fade>
+                </Modal>
                 <TableContainer component={Paper} sx={{ maxHeight: "calc(100vh - 200px)" }}>
                     <Table stickyHeader size="small">
                         <TableHead >
                             <TableRow>
                                 <HeaderCell>{`Time: ${timezone}`}</HeaderCell>
-                                <HeaderCell>Commander</HeaderCell>
+                                <HeaderCell align={"center"}>Commander</HeaderCell>
                             </TableRow>
                         </TableHead>
                         <TableBody>
-                            {state.rows.map((row, index) => (row.account === null) ?
+                            { state.rows.map((row, index) => (row.account === null) ?
                                 <BodyRow key={row.timestamp} hover>
                                     <TableCell component="th" scope="row">
                                         <i>{row.dateString}</i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{row.timeString}
                                     </TableCell>
-                                    <TableCell>Reserve</TableCell>
+                                    <TableCell align={"center"}>Reserve</TableCell>
                                 </BodyRow> :
                                 <BodyRow key={row.timestamp}>
                                     <TableCell component="th" scope="row">
                                         <i>{row.dateString}</i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{row.timeString}
                                     </TableCell>
-                                    <TableCell>row.account</TableCell>
+                                    <TableCell align={"center"}>row.account</TableCell>
                                 </BodyRow>
                             )}
                         </TableBody>
